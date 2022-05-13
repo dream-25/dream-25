bf8bd29aa09b372a6c7cf7230272270237e0ce7a
dream-25 <biswasmohan2529@gmail.com>
2022-05-13 18:14:22 +0530

    @ItsMohan025   05/13/2022  18:14:22.66 PM

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import '/utils/routes.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 2000));
+    await Navigator.pushReplacementNamed(context, MyRoutes.homeRootRoute);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.red.shade100,
+            Colors.white,
+          ],
+        )),
+        child: Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.all(100.0),
+              child: Image.asset("assets/images/logo_text.png"),
+            )),
+      ),
+    );
   }
 }
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,14 +1,5 @@
 class MyRoutes {
   static String rootRoute = "/";
   static String splashRoute = "/user_splash";
-  static String loginNumberRoute = "/user_login_number";
-  static String signupRoute = "/user_signup";
   static String homeRootRoute = "/home_root";
-  static String homePageRoute = "/home_page";
-  static String cartPageRoute = "/cart_page";
-  static String categoryPageRoute = "/category_page";
-  static String profilePageRoute = "/profile_page";
-  static String searchPageRoute = "/search_page";
-  static String productSingleRoute = "/productSingle";
-  static String subCategory = "/sub_category";
 }
