0bc82114264d679fb1178a95a1c4be9a1ac05afa
dream-25 <biswasmohan2529@gmail.com>
2022-05-13 17:32:55 +0530

    @harshitAmar   05/13/2022  17:32:55.00 PM

---
+++ android/app/src/main/kotlin/com/dreamapp/drm25/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dreamapp.drm25
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ deploy.cmd
@@ -0,0 +1,10 @@
+
+cls
+for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
+for /F "tokens=2" %%i in ('time /t') do set mytimeam=%%i
+set mytime=%time%
+git add .
+git commit . -m "@harshitAmar   %mydate%  %mytime% %mytimeam%"
+git push -f origin main
+git push -f upstream main
+echo "Deploy Done"
